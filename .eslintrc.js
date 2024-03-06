b9bb4ad451014d9f3bf4ae4cf2c88790ed4023f4
Jorge Assaf <jorgeassaf160@gmail.com>
2024-03-05 19:29:34 -0600

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    'react-native/no-inline-styles': 0,
+    'no-undef': 0,
+    'no-unused-vars': 0,
+    'no-shadow': 0,
+    'react/react-in-jsx-scope': 0,
+  },
 };
